f50a5fcbc878bd8bc2d845e21b61e541f33589aa
Jieun-yyang <poohv7@gmail.com>
2021-03-15 17:20:56 +0900

    카카오 빌런즈 초기세팅 완료

---
+++ Desktop/kakao/src/pages/Main/Main.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Main.scss";
+
+class Main extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="mainContainer">123</div>
+        <button>클릭</button>
+      </div>
+    );
+  }
+}
+
+export default Main;
